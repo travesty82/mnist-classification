0db175fbea2b647584a267a9e62c1d9307d34d53
Indragie Karunaratne <i@indragie.com>
2015-11-11 20:30:18 -0700

    Perceptron training

--- main.m
+++ main.m
@@ -2,3 +2,7 @@ addpath('third-party/mnistHelper');
 
 train_images = loadMNISTImages('mnist/train-images-idx3-ubyte');
 train_labels = loadMNISTLabels('mnist/train-labels-idx1-ubyte');
+
+x = [1 0 0; 1 0 1; 1 1 0; 1 1 1];
+t = [1 1 1 0];
+train_perceptron(x, t, 0.1, @unipolar_tfn)
